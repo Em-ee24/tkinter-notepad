e753e05b0c532c8a4d16b68bc4ab68c2125c3781
MosElsh <moselsh1@gmail.com>
2022-10-20 18:21:14 +0100

    Added Style Configurations

--- styles.py
+++ styles.py
@@ -1,5 +1,14 @@
-import tkinter, detect
+import tkinter, detect, darkdetect
 from tkinter import ttk
 
 font = "Segoe UI Variable" if detect.windows else "San Fransisco"
 mac = True if detect.mac else False
+
+cursor = "pointinghand" if mac else "@Windows_Hand.cur"
+
+dark = darkdetect.isDark()
+
+menu_background = "#202020" if dark else "#f3f3f3"
+background = "#272727" if dark else "#f9f9f9"
+active_menu_background = "#2d2d2d" if dark else "#eaeaea"
+foreground = "white" if dark else "black"
