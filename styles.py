fc092d9f7059c6a30f958c95ce56ac340f0a6c41
MosElsh <moselsh1@gmail.com>
2022-11-03 19:12:36 +0000

    Updated Styles

--- styles.py
+++ styles.py
@@ -26,4 +26,6 @@ dark = darkdetect.isDark()
 menu_background = "#202020" if dark else "#f3f3f3"
 background = "#272727" if dark else "#f9f9f9"
 active_menu_background = "#2d2d2d" if dark else "#eaeaea"
+menu_command_background = active_menu_background
+active_menu_command_background = "#383838" if dark else "#f0f0f0"
 foreground = "white" if dark else "black"
